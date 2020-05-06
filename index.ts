804876d7e5a4045d991d9a64506bc454444946d8
John Papa <john@johnpapa.net>
2020-05-06 13:46:23 -0400

    .

--- index.ts
+++ index.ts
@@ -12,6 +12,14 @@ export async function foo() {
 
 export async function createIssueComment(comment: Octokit.Octokit.IssuesCreateCommentParams) {
   const auth = process.env.githubkey;
+  /**
+   * This is odd using Octokit.Octokit.
+   *
+   * Step 1) put your cursor on the function name.
+   * Step 2) open the Quick Tips
+   * Step 3) move to another file
+   * Step 4) Notice the import statement does not come over.
+   */
   const octokit = new Octokit.Octokit({ auth });
   const response = await octokit.issues.createComment(comment);
   return response;
